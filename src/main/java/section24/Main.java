9b4b619534e253758d56057075e0d4fbaebfbe98
lyg0114 <whdnseowkd@naver.com>
2023-04-07 11:19:56 +0900

    set thread name

--- src/main/java/section24/Main.java
+++ src/main/java/section24/Main.java
@@ -11,12 +11,13 @@ public class Main {
     Thread thread = new Thread(() -> {
       System.out.println("we are now in thread : " + Thread.currentThread().getName());
     });
+    thread.setName("New Worker Thread");
 
     System.out.println("we are now in thread : " + Thread.currentThread().getName()
-        + " before starting a new thread");
+        + " ## before starting a new thread");
     thread.start();
     System.out.println("we are now in thread : " + Thread.currentThread().getName()
-        + " after starting a new thread");
+        + " ## after starting a new thread");
     Thread.sleep(1000);
   }
 }
