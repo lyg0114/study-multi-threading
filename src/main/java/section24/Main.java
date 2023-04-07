99f43eda4fb2241033664b77f3b11038e1c68353
lyg0114 <whdnseowkd@naver.com>
2023-04-07 11:11:42 +0900

    create new thread

---
+++ src/main/java/section24/Main.java
@@ -0,0 +1,22 @@
+package section24;
+
+/**
+ * @author : iyeong-gyo
+ * @package : section24
+ * @since : 2023/04/07
+ */
+public class Main {
+
+  public static void main(String[] args) throws InterruptedException {
+    Thread thread = new Thread(() -> {
+      System.out.println("we are now in thread : " + Thread.currentThread().getName());
+    });
+
+    System.out.println("we are now in thread : " + Thread.currentThread().getName()
+        + " before starting a new thread");
+    thread.start();
+    System.out.println("we are now in thread : " + Thread.currentThread().getName()
+        + " after starting a new thread");
+    Thread.sleep(1000);
+  }
+}
