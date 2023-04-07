6786b7d918583bc533658513e365ec92bdfffcff
lyg0114 <whdnseowkd@naver.com>
2023-04-07 14:11:13 +0900

    add HackerClass

--- src/main/java/section25/VaultExample.java
+++ src/main/java/section25/VaultExample.java
@@ -7,6 +7,8 @@ package section25;
  */
 public class VaultExample {
 
+  public static final int MAX_PASSORD = 9999;
+
   public static void main(String[] args) {
 
   }
@@ -28,5 +30,57 @@ public class VaultExample {
       return this.password == guess;
     }
   }
+
+  private static abstract class HackerThread extends Thread {
+
+    protected Vault vault;
+
+    public HackerThread(Vault vault) {
+      this.vault = vault;
+      this.setName(this.getClass().getSimpleName());
+      this.setPriority(Thread.MAX_PRIORITY);
+    }
+
+    @Override
+    public synchronized void start() {
+      System.out.println("Starting thread " + this.getName());
+      super.start();
+    }
+  }
+
+  private static class AscendingHackerThread extends HackerThread {
+
+    public AscendingHackerThread(Vault vault) {
+      super(vault);
+    }
+
+    @Override
+    public void run() {
+      for (int guess = 0; guess < MAX_PASSORD; guess++) {
+        if (vault.isCorrectPassword(guess)) {
+          System.out.println(this.getName() + " guessed the password " + guess);
+          System.exit(0);
+        }
+      }
+    }
+  }
+
+  private static class DecendingHackerThread extends HackerThread {
+
+    public DecendingHackerThread(Vault vault) {
+      super(vault);
+    }
+
+    @Override
+    public void run() {
+      for (int guess = MAX_PASSORD; guess >= 0; guess--) {
+        if (vault.isCorrectPassword(guess)) {
+          System.out.println(this.getName() + " guessed the password " + guess);
+          System.exit(0);
+        }
+      }
+    }
+  }
+
 
 }
