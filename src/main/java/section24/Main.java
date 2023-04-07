5c7771bc0847cb90886d7235907e81ab31eda446
lyg0114 <whdnseowkd@naver.com>
2023-04-07 12:01:52 +0900

    refactoring

--- src/main/java/section24/Main.java
+++ src/main/java/section24/Main.java
@@ -10,14 +10,18 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
     Thread thread = new Thread(() -> {
       System.out.println("we are now in thread : " + Thread.currentThread().getName());
+      System.out.println("Current thread pritiroit is : " + Thread.currentThread().getPriority());
     });
+
     thread.setName("New Worker Thread");
+    thread.setPriority(Thread.MAX_PRIORITY);
 
     System.out.println("we are now in thread : " + Thread.currentThread().getName()
         + " ## before starting a new thread");
     thread.start();
     System.out.println("we are now in thread : " + Thread.currentThread().getName()
         + " ## after starting a new thread");
+
     Thread.sleep(1000);
   }
 }
