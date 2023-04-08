ed1bb6aa68789602c0ec22ce6f1c69275d2fc4a7
lyg0114 <whdnseowkd@naver.com>
2023-04-08 10:38:28 +0900

    complete InterruptThread2

--- src/main/java/section37/InterruptThread2.java
+++ src/main/java/section37/InterruptThread2.java
@@ -9,11 +9,13 @@ import java.math.BigInteger;
  */
 public class InterruptThread2 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Thread thread = new Thread(
         new LongComputatonTask(new BigInteger("20000"), new BigInteger("1000000000")));
     thread.setName("big");
     thread.start();
+    thread.sleep(3000);
+    thread.interrupt();
   }
 
   private static class LongComputatonTask implements Runnable {
@@ -29,12 +31,15 @@ public class InterruptThread2 {
     @Override
     public void run() {
       System.out.println(base + "^" + power + " =" + pow(base, power));
-
     }
 
     private BigInteger pow(BigInteger base, BigInteger power) {
       BigInteger result = BigInteger.ONE;
       for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+        if (Thread.currentThread().isInterrupted()) {
+          System.out.println("current result : " + result);
+          return BigInteger.ZERO;
+        }
         result = result.multiply(base);
       }
       return result;
