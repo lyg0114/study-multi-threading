c43303aa26e3b8932479e5b39cdf510156986c57
lyg0114 <whdnseowkd@naver.com>
2023-04-08 10:10:36 +0900

    add interrupt method

--- src/main/java/section37/InterruptThread.java
+++ src/main/java/section37/InterruptThread.java
@@ -10,6 +10,7 @@ public class InterruptThread {
   public static void main(String[] args) {
     Thread thread = new Thread(new BlockingTask());
     thread.start();
+    thread.interrupt();
   }
 
   public static class BlockingTask implements Runnable {
