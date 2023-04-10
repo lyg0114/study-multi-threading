0c72c44a454194484c7c44edf04dd27f5d46f0ea
lyg0114 <whdnseowkd@naver.com>
2023-04-10 11:04:02 +0900

    add MinMaxMetrics class

---
+++ src/main/java/section617/example/MinMaxMetrics.java
@@ -0,0 +1,40 @@
+package section617.example;
+
+/**
+ * @author : iyeong-gyo
+ * @package : section617.example
+ * @since : 2023/04/10
+ */
+public class MinMaxMetrics {
+
+
+  // Add all necessary member variables
+
+  /**
+   * Initializes all member variables
+   */
+  public MinMaxMetrics() {
+    // Add code here
+  }
+
+  /**
+   * Adds a new sample to our metrics.
+   */
+  public void addSample(long newSample) {
+    // Add code here
+  }
+
+  /**
+   * Returns the smallest sample we've seen so far.
+   */
+  public long getMin() {
+    // Add code here
+  }
+
+  /**
+   * Returns the biggest sample we've seen so far.
+   */
+  public long getMax() {
+    // Add code here
+  }
+}
