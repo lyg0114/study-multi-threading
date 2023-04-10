9e7c5b4ed740de885816481d7eac043bc426711e
lyg0114 <whdnseowkd@naver.com>
2023-04-10 11:11:50 +0900

    complete MinMaxMetrics

--- src/main/java/section617/example/MinMaxMetrics.java
+++ src/main/java/section617/example/MinMaxMetrics.java
@@ -7,34 +7,32 @@ package section617.example;
  */
 public class MinMaxMetrics {
 
+  private volatile long minValue;
+  private volatile long maxValue;
 
-  // Add all necessary member variables
-
-  /**
-   * Initializes all member variables
-   */
   public MinMaxMetrics() {
-    // Add code here
+    this.maxValue = Long.MIN_VALUE;
+    this.minValue = Long.MAX_VALUE;
   }
 
-  /**
-   * Adds a new sample to our metrics.
-   */
   public void addSample(long newSample) {
-    // Add code here
+    synchronized (this) {
+      this.minValue = Math.min(newSample, this.minValue);
+      this.maxValue = Math.max(newSample, this.maxValue);
+    }
   }
 
   /**
    * Returns the smallest sample we've seen so far.
    */
   public long getMin() {
-    // Add code here
+    return this.minValue;
   }
 
   /**
    * Returns the biggest sample we've seen so far.
    */
   public long getMax() {
-    // Add code here
+    return this.maxValue;
   }
 }
