c6f1071cd85d60bbda1b485574564c48b4a824a8
lyg0114 <whdnseowkd@naver.com>
2023-06-27 13:11:23 +0900

    Add : ConcurrentHashMapThreadSafetyExample

---
+++ src/main/java/study/ConcurrentHashMapThreadSafetyExample.java
@@ -0,0 +1,43 @@
+package study;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author : iyeong-gyo
+ * @package : study
+ * @since : 2023/06/27
+ */
+
+public class ConcurrentHashMapThreadSafetyExample {
+
+  public static void main(String[] args) throws InterruptedException {
+    ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<>();
+
+    // 여러 스레드가 동시에 맵에 요소를 추가하는 작업 수행
+    Runnable task = () -> {
+      for (int i = 0; i < 1000; i++) {
+        map.put(Thread.currentThread().getName() + "-" + i, i);
+      }
+    };
+
+    // 여러 스레드 생성 및 작업 제출
+    Thread thread1 = new Thread(task);
+    Thread thread2 = new Thread(task);
+    thread1.start();
+    thread2.start();
+
+    // 모든 작업이 완료될 때까지 대기
+    thread1.join();
+    thread2.join();
+
+    // 맵의 크기 확인
+    int size = map.size();
+    System.out.println("Map size: " + size);
+
+    // 맵의 모든 요소 출력
+    for (String key : map.keySet()) {
+      System.out.println(key + ": " + map.get(key));
+    }
+  }
+}
+
