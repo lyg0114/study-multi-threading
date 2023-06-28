0cd338a120d77a154325e43015d449a190abc02b
lyg0114 <whdnseowkd@naver.com>
2023-06-28 10:22:52 +0900

    Add : CoreCount

---
+++ src/main/java/study/CoreCount.java
@@ -0,0 +1,14 @@
+package study;
+
+/**
+ * @author : iyeong-gyo
+ * @package : study
+ * @since : 2023/06/27
+ */
+public class CoreCount {
+
+  public static void main(String[] args) {
+    int i = Runtime.getRuntime().availableProcessors();
+    System.out.println("i = " + i);
+  }
+}
