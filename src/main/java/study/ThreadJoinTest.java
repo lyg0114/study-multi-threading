e859589036b409e9ffc5613d8faa1a90c5fa9964
lyg0114 <whdnseowkd@naver.com>
2023-07-22 16:22:13 +0900

    Update : refactoring ThreadJoinTest

--- src/main/java/study/ThreadJoinTest.java
+++ src/main/java/study/ThreadJoinTest.java
@@ -12,21 +12,26 @@ public class ThreadJoinTest {
 
   public static void main(String[] args) throws InterruptedException {
     Thread t1 = new Thread(() -> sum(1, 20));
-    t1.start(); t1.join();
+    t1.start();
+    t1.join();
     Thread t2 = new Thread(() -> sum(21, 40));
-    t2.start(); t2.join();
+    t2.start();
+    t2.join();
     Thread t3 = new Thread(() -> sum(41, 60));
-    t3.start(); t3.join();
+    t3.start();
+    t3.join();
     Thread t4 = new Thread(() -> sum(61, 80));
-    t4.start(); t4.join();
+    t4.start();
+    t4.join();
     Thread t5 = new Thread(() -> sum(81, 100));
-    t5.start(); t5.join();
+    t5.start();
+    t5.join();
 
     System.out.println("sum = " + sum);
   }
 
-  public static synchronized void sum(int start, int end) {
-    for (int i = start; i < end; i++) {
+  public static void sum(int start, int end) {
+    for (int i = start; i <= end; i++) {
       sum += i;
     }
   }
